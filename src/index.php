a09f56fc8aa1a9cbb88eef0974639c96fba81b88
Max Trautwein <trautwein.max@gmail.com>
2026-05-03 16:26:20 +0200

    Improved layout

--- src/index.php
+++ src/index.php
@@ -9,28 +9,44 @@
     <script src="index.js?v=<?php echo filemtime('index.js'); ?>"></script>
 </head>
 <body>
-<h1>Overview</h1>
+<header>
+    <h1>Overview</h1>
+</header>
 <main>
-    <div class="app-list-container" >
-        <div id="app-list-header">
-            <input type="checkbox" checked="checked" class="btn-check" id="toggle-sample" autocomplete="off">
-            <label class="btn" for="toggle-sample"><span class='badge text-bg-secondary text-bg-info'>Sample</span></label>
+    <?php
+    require_once 'ApplicationUtil.php';
+    $util = new ApplicationUtil();
+    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-5">
+                <input type="checkbox" checked="checked" class="btn-check" id="toggle-sample" autocomplete="off">
+                <label class="btn" for="toggle-sample"><span class='badge text-bg-secondary text-bg-info'>Sample</span></label>
 
-            <input type="checkbox" checked="checked"  class="btn-check" id="toggle-team" autocomplete="off">
-            <label class="btn" for="toggle-team"><span class='badge text-bg-secondary text-bg-success'>Team</span></label>
+                <input type="checkbox" checked="checked" class="btn-check" id="toggle-team" autocomplete="off">
+                <label class="btn" for="toggle-team"><span
+                            class='badge text-bg-secondary text-bg-success'>Team</span></label>
+            </div>
+            <div class="col">
+                <h2 id="iframeUrlDisplay"><?php echo $util->FirstAppUrl(false) ?></h2>
+            </div>
         </div>
-        <div  class="app-list">
-            <?php
-            require_once 'ApplicationUtil.php';
-            $util = new ApplicationUtil();
-            $util->displayAllApplications();
-            ?>
+        <div class="row h-100">
+            <div class="col-5 max-h-100">
+                <div class="app-list max-h-100">
+                    <?php
+                    $util->displayAllApplications();
+                    ?>
+                </div>
+            </div>
+            <div class="col">
+                    <iframe src="<?php echo $util->FirstAppUrl(true) ?>"></iframe>
+            </div>
         </div>
-    </div>
-    <div class="preview">
-        <h2 id="iframeUrlDisplay"><?php echo $util->FirstAppUrl(false) ?></h2>
-        <iframe src="<?php echo $util->FirstAppUrl(true) ?>"></iframe>
     </div>
 </main>
+<footer>
+
+</footer>
 </body>
 </html>
